ff56f1f045e2c53b8f95e6ff85f78e15ec16d7c4
anugoyal998 <anugoyal998@gmail.com>
2022-02-11 17:55:09 +0530

    inital commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        myGreen: '#03A48B',
+        myGreenFaded: '#E0F4F1',
+        myGray1: '#F0F3F6',
+        myGray2: '#EDF0F5',
+        myGray3: '#D0D3E3'
+      }
+    },
+  },
+  plugins: [],
+}
