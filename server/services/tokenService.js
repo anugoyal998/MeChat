797b3af3bce6d424dc69355d06d7047f9b8c5f5f
anugoyal998 <anugoyal998@gmail.com>
2022-10-28 11:50:13 +0530

    sfdsd

--- server/services/tokenService.js
+++ server/services/tokenService.js
@@ -1,54 +1,43 @@
-const jwt = require("jsonwebtoken")
-const accessTokenSecret = process.env.JWT_ACCESS_TOKEN_SECRET
-const refreshTokenSecret = process.env.JWT_REFRESH_TOKEN_SECRET
-const refreshModel = require("../model/refresh")
+const jwt = require("jsonwebtoken");
+const accessTokenSecret = process.env.JWT_ACCESS_TOKEN_SECRET;
+const refreshTokenSecret = process.env.JWT_REFRESH_TOKEN_SECRET;
+const refreshModel = require("../model/refresh");
 
-class TokenService{
-    generateTokens(payload){
-        let accessToken;
-        let refreshToken;
-        jwt.sign(payload,accessTokenSecret,{expiresIn: '1d'},(err,token)=> {
-            if(err)return null;
-            accessToken = token;
-        })
-        jwt.sign(payload,refreshTokenSecret,{expiresIn: '7d'},(err,token)=> {
-            if(err)return null;
-            refreshToken = token;
-        })
-        return {accessToken, refreshToken}
+class TokenService {
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, accessTokenSecret, {
+      expiresIn: "1d",
+    });
+    const refreshToken = jwt.sign(payload, refreshTokenSecret, {
+      expiresIn: "7d",
+    });
+    return { accessToken, refreshToken };
+  }
+  async storeRefreshToken(token, userId) {
+    const res = await refreshModel.findOne({ userId });
+    if (res) {
+      return await refreshModel.updateOne({ userId }, { token });
     }
-    async storeRefreshToken(token,userId){
-        const res = await refreshModel.findOne({userId})
-        if(res){
-            return await refreshModel.updateOne({userId},{token})
-        }
-        return await refreshModel.create({token,userId})
-    }
-    verifyAccessToken(token){
-        let data;
-        jwt.verify(token,accessTokenSecret,(err,decoded)=> {
-            if(err)return null;
-            data = decoded;
-        })
-        return data;
-    }
-    verifyRefreshToken(refreshToken){
-        let data;
-        jwt.verify(refreshToken,refreshTokenSecret,(err,decoded)=> {
-            if(err)return null;
-            data = decoded;
-        })
-        return data;
-    }
-    async findRefreshToken(userId){
-        return await refreshModel.findOne({userId: userId})
-    }
-    async updateRefreshToken(userId,refreshToken){
-        return await refreshModel.updateOne({userId: userId},{token: refreshToken})
-    }
-    async removeToken(refreshToken){
-        return await refreshModel.deleteOne({token: refreshToken})
-    }
+    return await refreshModel.create({ token, userId });
+  }
+  verifyAccessToken(token) {
+    return jwt.verify(token, accessTokenSecret);
+  }
+  verifyRefreshToken(refreshToken) {
+    return jwt.verify(refreshToken, refreshTokenSecret);
+  }
+  async findRefreshToken(userId) {
+    return await refreshModel.findOne({ userId: userId });
+  }
+  async updateRefreshToken(userId, refreshToken) {
+    return await refreshModel.updateOne(
+      { userId: userId },
+      { token: refreshToken }
+    );
+  }
+  async removeToken(refreshToken) {
+    return await refreshModel.deleteOne({ token: refreshToken });
+  }
 }
 
-module.exports = new TokenService()
+module.exports = new TokenService();
